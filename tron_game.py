203ce1ee8cb0315ea808d395de66d501705923a4
iveri99 <lukacho.iv@gmail.com>
2025-01-25 10:09:27 +0000

    Have completely refactored code into an OOP style.

--- tron_game.py
+++ tron_game.py
@@ -39,7 +39,6 @@ class Move:
         self.y = y
         self.speed = speed
         self.direction = direction
-        self.previous_positions = []
 
         self.trail = [] # Keep track of position - prevent self collision
         self.max_trail_length = MAX_TRAIL_LENGTH # Limit length of trail
@@ -49,9 +48,6 @@ class Move:
         # Checking if self.direction has been given a value
         if self.direction is None:
             raise ValueError("Direction must be set before calling move.")
-        
-        # Save current position to trail
-        self.previous_positions.append((self.x, self.y))
 
         # Update position based on direction
         if self.direction == "UP":
@@ -63,6 +59,7 @@ class Move:
         elif self.direction == "RIGHT":
             self.x += self.speed
 
+        # Add current position to the trail
         self.trail.append((self.x, self.y))
         if len(self.trail) > self.max_trail_length:
             self.trail.pop(0)    
@@ -76,14 +73,12 @@ class Move:
             "RIGHT": "LEFT"
         }
 
-        if new_direction != opposite_directions[self.direction]:
+        if new_direction is None or new_direction != opposite_directions[self.direction]:
             self.direction = new_direction
 
-    def check_collision(self):
+    def check_wall_collision(self):
         # Check if out of bounds
         if self.x < 0 or self.x >= WIDTH or self.y < 0 or self.y >= HEIGHT:
-            return True
-        elif (self.x, self.y) in self.previous_positions:
             return True
         return False
     
@@ -99,9 +94,8 @@ class Move:
             
 class Player(Move):
     # Initialise Player class
-    def __init__(self):
-        self.speed = CELL_SIZE // 5
-        self.target_position = None
+    def __init__(self, x, y, speed=5, direction = None):
+        super().__init__(x, y, speed, direction)
 
     def move(self):
         self.update_position()
@@ -111,7 +105,8 @@ class Player(Move):
 
 class UserPlayer(Player):
     def __init__(self, x, y, colour = BLUE, speed=5, direction="LEFT"):
-        super().__init__(x, y, colour, speed, direction)
+        super().__init__(x, y, speed, direction)
+        self.colour = colour
 
     def handle_input(self, keys):
         # Example of using arrow keys
@@ -128,43 +123,47 @@ class UserPlayer(Player):
         # Draw trail with fading + player's square
         for i, pos in enumerate(self.trail):
             alpha = int(255 * (i / self.max_trail_length))
-            trail_color = (self.colour, alpha)  # RGBA
-            trail_rect = pygame.Surface((10, 10), pygame.SRCALPHA)
-            trail_rect.fill(trail_color)
+            trail_colour = (*self.colour, alpha)  # RGBA
+            trail_rect = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE), pygame.SRCALPHA)
+            trail_rect.fill(trail_colour)
             surface.blit(trail_rect, pos)
+
+        pygame.draw.rect(surface, self.colour, (self.x, self.y, PLAYER_SIZE, PLAYER_SIZE))
 
 class ProgramPlayer(Player):
     def __init__(self, x, y, colour = RED, speed=5, direction= "RIGHT"):
-        super().__init__(x, y, colour, speed, direction)
+        super().__init__(x, y, speed, direction)
+        self.colour = colour
 
-class Main(UserPlayer, ProgramPlayer):
+    def decide_movement(self):
+        # Basic AI logic, start with making random turns etc.
+        pass
+
+    def draw(self, surface):
+        # Draw trail with fading + player's square
+        for i, pos in enumerate(self.trail):
+            alpha = int(255 * (i / self.max_trail_length))
+            trail_colour = (*self.colour, alpha)  # RGBA
+            trail_rect = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE), pygame.SRCALPHA)
+            trail_rect.fill(trail_colour)
+            surface.blit(trail_rect, pos)
+
+
+class Main():
     # Initialise Main
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("PyTron")
         self.clock = pygame.time.Clock()
+        
         self.running = True
         self.game_over = False
         self.font = pygame.font.Font(None, 74)
 
-    # Two players created - user and program
-        self.user = UserPlayer(100, HEIGHT // 2, BLUE, speed=5, direction="LEFT")
-        self.program = ProgramPlayer(WIDTH - 100, HEIGHT // 2, RED, speed=5, direction="RIGHT")
-
-    def run(self):
-        # Main game loop
-
-        while self.running:
-            self.handle_events()
-
-            if not self.game_over:
-                self.update()
-            
-            self.draw()
-            self.clock.tick(FPS)
-        
-        pygame.quit()
+        # Two players created - user and program
+        self.user = UserPlayer(x = WIDTH - 100, y = HEIGHT // 2, colour = BLUE, speed = 5, direction = "LEFT")
+        self.program = ProgramPlayer(x = 100, y = HEIGHT // 2, colour = RED, speed = 5, direction = "RIGHT")
 
     def handle_events(self):
         # Process game events frame by frame
@@ -172,25 +171,68 @@ class Main(UserPlayer, ProgramPlayer):
             if event.type == pygame.QUIT:
                 self.running = False
 
-    keys = pygame.key.get_pressed()
+        keys = pygame.key.get_pressed()
 
-    user_input = UserPlayer.handle_input()
-
-    # Add current position to trail
-    player_1_trail.append(player_1['position'][:])
-    player_2_trail.append(player_2['position'][:])
-
-    if len(player_1_trail) > MAX_TRAIL_LENGTH:
-        player_1_trail.pop(0) # Remove the oldest position
+        self.user.handle_input(keys)
+        self.program.decide_movement() # Program decides movement on its own
     
-    if len(player_2_trail) > MAX_TRAIL_LENGTH:
-        player_2_trail.pop(0) # Remove the oldest position
+    def update(self):
+        self.user.update_position()
+        self.program.update_position()
 
-    screen.fill(BLACK)
-    trail_surface.fill((0, 0, 0, 0))
+        # Checks for collisions
+        if self.user.check_wall_collision() or self.user.check_self_collision():
+            self.game_over = True
+        if self.program.check_wall_collision() or self.program.check_self_collision():
+            self.game_over = True
 
-    player_1_trail_colour = (*player_1['colour'], trail_alpha)
-    player_2_trail_colour = (*player_2['colour'], trail_alpha)
+        # Checks if user hits program's trail or vice versa
+        if self.user.get_position() in self.program.trail:
+            self.game_over = True
+        if self.program.get_position() in self.user.trail:
+            self.game_over = True
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        if self.game_over:
+            text = self.font.render("GAME OVER", True, (255, 255, 255))
+            rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+            self.screen.blit(text, rect)
+        else:
+            self.user.draw(self.screen)
+            self.program.draw(self.screen)
+        pygame.display.flip()
+    
+    def run(self):
+        # Game loop
+        while self.running:
+            self.handle_events()
+            if not self.game_over:
+                self.update()
+            self.draw()
+            self.clock.tick(FPS)
+        pygame.quit()
+
+   
+
+if __name__ == "__main__":
+    Main().run()
+
+    # # Add current position to trail
+    # player_1_trail.append(player_1['position'][:])
+    # player_2_trail.append(player_2['position'][:])
+
+    # if len(player_1_trail) > MAX_TRAIL_LENGTH:
+    #     player_1_trail.pop(0) # Remove the oldest position
+    
+    # if len(player_2_trail) > MAX_TRAIL_LENGTH:
+    #     player_2_trail.pop(0) # Remove the oldest position
+
+    # screen.fill(BLACK)
+    # trail_surface.fill((0, 0, 0, 0))
+
+    # player_1_trail_colour = (*player_1['colour'], trail_alpha)
+    # player_2_trail_colour = (*player_2['colour'], trail_alpha)
 
     # # Draw player 1 trail
     # for pos in player_1_trail:
@@ -201,38 +243,38 @@ class Main(UserPlayer, ProgramPlayer):
     #     pygame.draw.rect(screen, player_2_trail_colour, (pos[0], pos[1], PLAYER_SIZE, PLAYER_SIZE))
 
     # Draw Player 1 trail with fading effect
-    for i, pos in enumerate(player_1_trail):
-        # Calculate alpha based on position in the trail list (older segments are more transparent)
-        alpha = int(255 * (i / MAX_TRAIL_LENGTH))  # Older positions will have lower alpha
-        trail_color = (*player_1['colour'], alpha)
-        trail_rect = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE), pygame.SRCALPHA)
-        trail_rect.fill(trail_color)
-        screen.blit(trail_rect, pos)
+    # for i, pos in enumerate(player_1_trail):
+    #     # Calculate alpha based on position in the trail list (older segments are more transparent)
+    #     alpha = int(255 * (i / MAX_TRAIL_LENGTH))  # Older positions will have lower alpha
+    #     trail_color = (*player_1['colour'], alpha)
+    #     trail_rect = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE), pygame.SRCALPHA)
+    #     trail_rect.fill(trail_color)
+    #     screen.blit(trail_rect, pos)
     
-    # Draw Player 2 trail with fading effect
-    for i, pos in enumerate(player_2_trail):
-        alpha = int(255 * (i / MAX_TRAIL_LENGTH))
-        trail_color = (*player_2['colour'], alpha)
-        trail_rect = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE), pygame.SRCALPHA)
-        trail_rect.fill(trail_color)
-        screen.blit(trail_rect, pos)
+    # # Draw Player 2 trail with fading effect
+    # for i, pos in enumerate(player_2_trail):
+    #     alpha = int(255 * (i / MAX_TRAIL_LENGTH))
+    #     trail_color = (*player_2['colour'], alpha)
+    #     trail_rect = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE), pygame.SRCALPHA)
+    #     trail_rect.fill(trail_color)
+    #     screen.blit(trail_rect, pos)
 
-    if (player_1['position'][0] < 0 or 
-        player_1['position'][0] >= WIDTH or
-        player_1['position'][1] < 0 or 
-        player_1['position'][1] >= HEIGHT):
-        print("Player 1 hit the wall!")
-        game_over = True
+    # if (player_1['position'][0] < 0 or 
+    #     player_1['position'][0] >= WIDTH or
+    #     player_1['position'][1] < 0 or 
+    #     player_1['position'][1] >= HEIGHT):
+    #     print("Player 1 hit the wall!")
+    #     game_over = True
 
 
 
     screen.blit(trail_surface, (0, 0)) # Blit trail surface onto main screen
 
-    # Drawing sprites for players, player 1 as blue square, player 2 as red square
-    pygame.draw.rect(screen, player_1['colour'],
-                (player_1['position'][0], player_1['position'][1], PLAYER_SIZE, PLAYER_SIZE))  
-    pygame.draw.rect(screen, player_2['colour'], 
-                 (player_2['position'][0], player_2['position'][1], PLAYER_SIZE, PLAYER_SIZE))
+    # # Drawing sprites for players, player 1 as blue square, player 2 as red square
+    # pygame.draw.rect(screen, player_1['colour'],
+    #             (player_1['position'][0], player_1['position'][1], PLAYER_SIZE, PLAYER_SIZE))  
+    # pygame.draw.rect(screen, player_2['colour'], 
+    #              (player_2['position'][0], player_2['position'][1], PLAYER_SIZE, PLAYER_SIZE))
     
     pygame.display.flip() # Update display
 
